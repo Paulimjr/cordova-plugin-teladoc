20f1cbdc37866b8a07228fa66b69713ce6499fb8
Paulo Cesar <paulim.jr@hotmail.com>
2020-06-15 19:07:21 +0100

    Update removing the Teladoc.prototype in functions

--- www/teladocPlugin.js
+++ www/teladocPlugin.js
@@ -1,43 +1,44 @@
-var exec = require("cordova/exec");
+//var exec = require("cordova/exec");
 
 /**
- * Constructor.
+ * Innovagency - Team Mobile
  *
+ * Paulo Cesar & Pedro Remedios
  * @returns {Teladoc}
  */
 function Teladoc() {
     
 }
 
-Teladoc.prototype.teladocLogin = function(successCallback, errorCallback, encryptedKey) {
+exports.teladocLogin = function(successCallback, errorCallback, encryptedKey) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'doTeladocLogin', [encryptedKey]);
 };
            
-Teladoc.prototype.teladocLoginWithToken = function(successCallback, errorCallback, encryptedKey) {
+exports.teladocLoginWithToken = function(successCallback, errorCallback, encryptedKey) {
     exec(successCallback, errorCallback, 'TeladocPlugin', 'doTeladocLoginWithToken', [encryptedKey]);
 };
 
-Teladoc.prototype.teladocDashboard = function(successCallback, errorCallback) {
+exports.teladocDashboard = function(successCallback, errorCallback) {
     exec(successCallback, errorCallback, 'TeladocPlugin', 'showDashboard', []);
 };
                
-Teladoc.prototype.teladocImageUpload = function(successCallback, errorCallback) {
+exports.teladocImageUpload = function(successCallback, errorCallback) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'showImageUpload', []);
 };
 
-Teladoc.prototype.teladocConsultationList = function(successCallback, errorCallback) {
+exports.teladocConsultationList = function(successCallback, errorCallback) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'showConsultations', []);
 };
                
-Teladoc.prototype.teladocRequestConsultation = function(successCallback, errorCallback) {
+exports.teladocRequestConsultation = function(successCallback, errorCallback) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'requestConsultation', []);
 };
                
-Teladoc.prototype.teladocAccountSettings = function(successCallback, errorCallback) {
+exports.teladocAccountSettings = function(successCallback, errorCallback) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'showTeladocAccountSettings', []);
 };
 
-Teladoc.prototype.teladocLogout = function(successCallback, errorCallback) {
+exports.teladocLogout = function(successCallback, errorCallback) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'doTeladocLogout', []);
 };
                
@@ -45,25 +46,22 @@ Teladoc.prototype.getConsultations = function(successCallback, errorCallback) {
   exec(successCallback, errorCallback, 'TeladocPlugin', 'getTeladocConsultations', []);
 };
                
-Teladoc.prototype.setPrimaryColor = function(successCallback, errorCallback, color) {
+exports.setPrimaryColor = function(successCallback, errorCallback, color) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'setPrimaryColor', [color]);
 };
 
-Teladoc.prototype.setSecondaryColor = function(successCallback, errorCallback, color) {
+exports.setSecondaryColor = function(successCallback, errorCallback, color) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'setSecondaryColor', [color]);
 };
 
-Teladoc.prototype.setTertiaryColor = function(successCallback, errorCallback, color) {
+exports.setTertiaryColor = function(successCallback, errorCallback, color) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'setTertiaryColor', [color]);
 };
 
-Teladoc.prototype.setStatusBarColor = function(successCallback, errorCallback, color) {
+exports.setStatusBarColor = function(successCallback, errorCallback, color) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'setStatusBarColor', [color]);
 };
 
-Teladoc.prototype.changeColor = function(successCallback, errorCallback, colors) {
+exports.changeColor = function(successCallback, errorCallback, colors) {
    exec(successCallback, errorCallback, 'TeladocPlugin', 'changeColor', [colors]);
 };
-
-
-module.exports = new Teladoc();
