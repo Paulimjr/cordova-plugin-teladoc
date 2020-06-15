232e3a6c3bca3937897fd9ba3afe8159e212a7b9
Pedro Remedios <premedios@innovagency.com>
2020-06-15 17:55:36 +0100

    Plugin functions change

--- www/teladocPlugin.js
+++ www/teladocPlugin.js
@@ -2,49 +2,38 @@ var exec = require("cordova/exec");
 
 function Teladoc() {}
 
-Teladoc.prototype.teladocLoginWithToken = function (successCallback, errorCallback, encryptedKey) {
+exports.teladocLoginWithToken = function (successCallback, errorCallback, encryptedKey) {
     exec(successCallback, errorCallback, "TeladocPlugin", "doTeladocLoginWithToken", [encryptedKey]);
 };
 
-Teladoc.prototype.teladocDashboard = function (successCallback, errorCallback) {
+exports.teladocDashboard = function (successCallback, errorCallback) {
     exec(successCallback, errorCallback, "TeladocPlugin", "showDashboard", []);
 };
 
-Teladoc.prototype.teladocImageUpload = function (successCallback, errorCallback) {
+exports.teladocImageUpload = function (successCallback, errorCallback) {
     exec(successCallback, errorCallback, "TeladocPlugin", "showImageUpload", []);
 };
 
-Teladoc.prototype.teladocConsultationList = function (successCallback, errorCallback) {
+exports.teladocConsultationList = function (successCallback, errorCallback) {
     exec(successCallback, errorCallback, "TeladocPlugin", "showConsultations", []);
 };
 
-Teladoc.prototype.teladocRequestConsultation = function (successCallback, errorCallback) {
+exports.teladocRequestConsultation = function (successCallback, errorCallback) {
     exec(successCallback, errorCallback, "TeladocPlugin", "requestConsultation", []);
 };
 
-Teladoc.prototype.teladocAccountSettings = function (successCallback, errorCallback) {
+exports.teladocAccountSettings = function (successCallback, errorCallback) {
     exec(successCallback, errorCallback, "TeladocPlugin", "showTeladocAccountSettings", []);
 };
 
-Teladoc.prototype.teladocLogout = function (successCallback, errorCallback) {
+exports.teladocLogout = function (successCallback, errorCallback) {
     exec(successCallback, errorCallback, "TeladocPlugin", "doTeladocLogout", []);
 };
 
-Teladoc.prototype.getConsultations = function (successCallback, errorCallback) {
+exports.getConsultations = function (successCallback, errorCallback) {
     exec(successCallback, errorCallback, "TeladocPlugin", "getTeladocConsultations", []);
 };
 
-Teladoc.prototype.changeColor = function (successCallback, errorCallback, colors) {
+exports.changeColor = function (successCallback, errorCallback, colors) {
     exec(successCallback, errorCallback, "TeladocPlugin", "changeColor", [colors]);
 };
-
-Teladoc.install = function() {
-   if !(window.plugins) {
-      window.plugins = {}
-   }
-
-   window.plugins.Teladoc = new Teladoc();
-   return window.plugins.Teladoc;
-}
-
-cordova.addConstructor(Teladoc.install);
