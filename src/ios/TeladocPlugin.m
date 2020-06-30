c6c6a105d3c204131fe1099d882e99a60ab2707f
Pedro Remedios <premedios@innovagency.com>
2020-06-30 14:19:39 +0100

    constant value error

--- src/ios/TeladocPlugin.m
+++ src/ios/TeladocPlugin.m
@@ -48,7 +48,7 @@
         if (self.loginToken == nil) {
             //NSError *error = [[NSError alloc] initWithDomain:@"cordova.plugins.Teladoc" code:1 userInfo:@{NSLocalizedDescriptionKey:@"Please login before using this function"}];
             //[self sendErrorResult:error.localizedDescription withCode:error.code callbackId:command.callbackId];
-            [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CSVCommandStatus_ERROR messageAsString:@"Please login before using this function"] callbackId:command.callbackId];
+            [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Please login before using this function"] callbackId:command.callbackId];
         } else {
             [[Teladoc apiService] callRoute:TDRouteDashboard withToken:self.loginToken andCompletion:^(BOOL completed, UIViewController *viewController, NSError *error) {
                 if (completed && viewController) {
