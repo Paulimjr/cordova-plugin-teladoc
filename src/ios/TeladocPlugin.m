548de3ebbeb6face4f7aee7b4c0f6be13195654f
Pedro Remedios <premedios@innovagency.com>
2020-07-01 11:03:43 +0100

    Change plugin result back to text

--- src/ios/TeladocPlugin.m
+++ src/ios/TeladocPlugin.m
@@ -186,7 +186,7 @@
             }
         }
         
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:colors];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Colors set successfully"];
 
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
